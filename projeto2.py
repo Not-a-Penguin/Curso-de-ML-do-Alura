ca5a12850265deff6fe0e6f9295ef5a996afaf74
Not-a-Penguin <ruster1506@gmail.com>
2021-02-02 23:03:24 -0400

    utilizando stratify e seed para fazer teste replicável

--- projeto2.py
+++ projeto2.py
@@ -6,6 +6,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 
 modelo = LinearSVC()
+SEED = 20
 
 dados = pd.read_csv(
     "https://gist.githubusercontent.com/guilhermesilveira/2d2efa37d66b6c84a722ea627a897ced/raw/10968b997d885cbded1c92938c7a9912ba41c615/tracking.csv")
@@ -22,10 +23,11 @@ dados = dados.rename(columns=mapa)
 x = dados[['principal', 'como_funciona', 'contato']]
 y = dados['comprou']
 
-treino_x, teste_x, treino_y, teste_y = train_test_split(x, y, test_size= 0.25)
-print(treino_x)
+treino_x, teste_x, treino_y, teste_y = train_test_split(x, y, test_size=0.25, random_state=SEED, stratify=y)
 
 modelo.fit(treino_x, treino_y)
 previsoes = modelo.predict(teste_x)
 
 acuracia = accuracy_score(teste_y, previsoes)
+
+print(acuracia)

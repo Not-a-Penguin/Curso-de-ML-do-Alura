624266e1517de983702ff9e51243d1ba31eabe08
Not-a-Penguin <ruster1506@gmail.com>
2021-02-03 01:30:24 -0400

    Linha de base e linha de decisão / uso do matplotlib

--- projeto3.py
+++ projeto3.py
@@ -4,11 +4,13 @@ from sklearn.svm import LinearSVC
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 import numpy as np
+import matplotlib.pyplot as plt
 
-dados = pd.read_csv("https://gist.githubusercontent.com/guilhermesilveira/1b7d5475863c15f484ac495bd70975cf/raw/16aff7a0aee67e7c100a2a48b676a2d2d142f646/projects.csv")
+dados = pd.read_csv(
+    "https://gist.githubusercontent.com/guilhermesilveira/1b7d5475863c15f484ac495bd70975cf/raw/16aff7a0aee67e7c100a2a48b676a2d2d142f646/projects.csv")
 
 modelo = LinearSVC()
-SEED = 20
+SEED = 30
 
 a_renomear = {
     'expected_hours': 'horas_esperadas',
@@ -25,7 +27,7 @@ troca = {
 
 dados['finalizado'] = dados.nao_finalizado.map(troca)
 
-sns.scatterplot(x="horas_esperadas", y="preco", data=dados, hue="finalizado")
+# sns.scatterplot(x="horas_esperadas", y="preco", data=dados, hue="finalizado")
 
 x = dados[['horas_esperadas', 'preco']]
 y = dados['finalizado']
@@ -41,3 +43,25 @@ previsoes_de_base = np.ones(540)
 acuracia = accuracy_score(teste_y, previsoes_de_base) * 100
 print("Acurácia do algoritmo de baseline: ", acuracia)
 
+# sns.scatterplot(x="horas_esperadas", y="preco", data=teste_x, hue=teste_y)
+
+x_min = teste_x.horas_esperadas.min()
+x_max = teste_x.horas_esperadas.max()
+y_min = teste_x.preco.min()
+y_max = teste_x.preco.max()
+
+pixel = 100
+eixo_x = np.arange(x_min, x_max, (x_max - x_min) / pixel)
+eixo_y = np.arange(y_min, y_max, (y_max - y_min) / pixel)
+
+xx, yy = np.meshgrid(eixo_x, eixo_y)
+
+pontos = np.c_[xx.ravel(), yy.ravel()]
+
+z = modelo.predict(pontos)
+z = z.reshape(xx.shape)
+print(z)
+
+plt.contourf(xx, yy, z, alpha=0.3)
+plt.scatter(teste_x.horas_esperadas, teste_x.preco, c=teste_y)
+plt.show()

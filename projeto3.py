9ef94e659a5a133f421cd9ded3558deabb05c176
Not-a-Penguin <ruster1506@gmail.com>
2021-02-03 01:53:45 -0400

    Utilizando o SVC não linear

--- projeto3.py
+++ projeto3.py
@@ -1,16 +1,18 @@
 import pandas as pd
 import seaborn as sns
-from sklearn.svm import LinearSVC
+from sklearn.svm import SVC
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
+from sklearn.preprocessing import StandardScaler
 import numpy as np
 import matplotlib.pyplot as plt
 
 dados = pd.read_csv(
     "https://gist.githubusercontent.com/guilhermesilveira/1b7d5475863c15f484ac495bd70975cf/raw/16aff7a0aee67e7c100a2a48b676a2d2d142f646/projects.csv")
 
-modelo = LinearSVC()
-SEED = 30
+SEED = 5
+np.random.seed(SEED)
+modelo = SVC()
 
 a_renomear = {
     'expected_hours': 'horas_esperadas',
@@ -32,7 +34,12 @@ dados['finalizado'] = dados.nao_finalizado.map(troca)
 x = dados[['horas_esperadas', 'preco']]
 y = dados['finalizado']
 
-treino_x, teste_x, treino_y, teste_y = train_test_split(x, y, test_size=0.25, random_state=SEED, stratify=y)
+raw_treino_x, raw_teste_x, treino_y, teste_y = train_test_split(x, y, test_size=0.25, stratify=y)
+
+scaler = StandardScaler()
+scaler.fit(raw_treino_x)
+treino_x = scaler.transform(raw_treino_x)
+teste_x = scaler.transform(raw_teste_x)
 
 modelo.fit(treino_x, treino_y)
 previsoes = modelo.predict(teste_x)
@@ -40,15 +47,18 @@ previsoes = modelo.predict(teste_x)
 acuracia = accuracy_score(teste_y, previsoes)
 
 previsoes_de_base = np.ones(540)
-acuracia = accuracy_score(teste_y, previsoes_de_base) * 100
-print("Acurácia do algoritmo de baseline: ", acuracia)
+#acuracia = accuracy_score(teste_y, previsoes_de_base) * 100
+print("Acurácia do algoritmo: ", acuracia)
 
 # sns.scatterplot(x="horas_esperadas", y="preco", data=teste_x, hue=teste_y)
 
-x_min = teste_x.horas_esperadas.min()
-x_max = teste_x.horas_esperadas.max()
-y_min = teste_x.preco.min()
-y_max = teste_x.preco.max()
+data_x = teste_x[:, 0]
+data_y = teste_x[:, 1]
+
+x_min = data_x.min()
+x_max = data_x.max()
+y_min = data_y.min()
+y_max = data_y.max()
 
 pixel = 100
 eixo_x = np.arange(x_min, x_max, (x_max - x_min) / pixel)
@@ -60,8 +70,7 @@ pontos = np.c_[xx.ravel(), yy.ravel()]
 
 z = modelo.predict(pontos)
 z = z.reshape(xx.shape)
-print(z)
 
 plt.contourf(xx, yy, z, alpha=0.3)
-plt.scatter(teste_x.horas_esperadas, teste_x.preco, c=teste_y)
+plt.scatter(data_x, data_y, c=teste_y, s=1)
 plt.show()

96af27ba4c02eb0f70bdaee766d060cc5b29dc82
Not-a-Penguin <ruster1506@gmail.com>
2021-02-04 01:24:05 -0400

    arvore de decisoes

--- projeto4.py
+++ projeto4.py
@@ -1,5 +1,12 @@
 import pandas as pd
+import numpy as np
 from datetime import datetime
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.tree import export_graphviz
+import graphviz
+from sklearn.metrics import accuracy_score
 
 uri = "https://gist.githubusercontent.com/guilhermesilveira/4d1d4a16ccbf6ea4e0a64a38a24ec884/raw/afd05cb0c796d18f3f5a6537053ded308ba94bf7/car-prices.csv"
 dados = pd.read_csv(uri)
@@ -28,3 +35,18 @@ dados = dados.drop(columns=['Unnamed: 0', 'milhas_por_ano', 'ano_do_modelo'], ax
 
 x = dados[['preco', 'idade_do_modelo', 'kilometros_por_ano']]
 y = dados['vendido']
+
+SEED = 5
+np.random.seed(SEED)
+raw_treino_x, raw_teste_x, treino_y, teste_y = train_test_split(x, y, test_size=0.25, stratify=y)
+#print("Treinaremos com %d elementos e testaremos com %d elementos" % (len(treino_x), len(teste_x)))
+
+modelo = DecisionTreeClassifier(max_depth=4)
+modelo.fit(raw_treino_x, treino_y)
+previsoes = modelo.predict(raw_teste_x)
+
+acuracia = accuracy_score(teste_y, previsoes) * 100
+print("A acurácia foi %.2f%%" % acuracia)
+dot_data = export_graphviz(modelo, out_file=None, feature_names=x.columns, filled=True, rounded= True, class_names=['não', 'sim'])
+grafico = graphviz.Source(dot_data, format='png')
+grafico.view()

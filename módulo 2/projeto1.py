59069346fb2094df3b9a422628af0c37e05b2637
Not-a-Penguin <ruster1506@gmail.com>
2021-02-04 20:47:04 -0400

    visualizando centroide com seaborn

--- módulo 2/projeto1.py
+++ módulo 2/projeto1.py
@@ -1,6 +1,9 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 from sklearn.preprocessing import StandardScaler
 from sklearn.cluster import KMeans
+from sklearn.manifold import TSNE
+import seaborn as sns
 
 uri = "https://raw.githubusercontent.com/alura-cursos/machine-learning-algoritmos-nao-supervisionados/master/movies.csv"
 
@@ -17,4 +20,17 @@ generos_escalados = scaler.fit_transform(generos)
 
 modelo = KMeans(n_clusters=3)
 modelo.fit(generos_escalados)
-print(modelo.labels_)
+grupos = pd.DataFrame(modelo.cluster_centers_, columns=generos.columns)
+grupos.transpose().plot.bar(subplots=True, figsize=(25, 25), sharex=False)
+
+grupo = 0
+filtro = modelo.labels_ == grupo
+dados_dos_filmes[filtro].sample(10)
+
+tsne = TSNE()
+visualizacao = tsne.fit_transform(generos_escalados)
+
+sns.set(rc={'figure.figsize': (13, 13)})
+
+a = sns.scatterplot(x=visualizacao[:, 0], y=visualizacao[:, 1], hue=modelo.labels_, palette=sns.color_palette('Set1', 3))
+plt.show()

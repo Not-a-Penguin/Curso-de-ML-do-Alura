f9b37a102cdd3a965402f53da9872b1bed41ba3a
Eduardo <ruster1506@gmail.com>
2021-02-17 10:53:42 -0400

    continuação do modulo 6

---
+++ modulo 6/projeto_parte_2.py
@@ -0,0 +1,32 @@
+import pandas as pd
+from sklearn.preprocessing import Normalizer
+from sklearn.cluster import KMeans
+from sklearn.metrics import silhouette_score
+
+#importando os dados e criando o dataframe
+uri = "https://raw.githubusercontent.com/alura-cursos/alura-clustering-validation/base-de-dados/CC%20GENERAL.csv"
+dataframe = pd.read_csv(uri)
+
+#remover atributos desnecessários
+dataframe.drop(columns=['CUST_ID', 'TENURE'], inplace=True)
+
+#achar dados que estão faltando
+missing = dataframe.isna().sum
+#print(missing)
+
+#substituir os valore faltantes pela mediana
+dataframe.fillna(dataframe.median(), inplace=True)
+
+#normalizar os dados
+values = Normalizer().fit_transform(dataframe.values)
+#print(values)
+
+#criando os clusters
+kmeans = KMeans(n_clusters=5, n_init=10, max_iter=300)
+y_pred = kmeans.fit_predict(values)
+
+#achar o coeficiente de silhueta
+labels = kmeans.labels_
+silhouette = silhouette_score(values, labels=labels, metric="euclidean")
+print(silhouette)
+
